9fdcc9ce3ef03cfc1e5481671340700a6f419534
mani703 <mani703@naver.com>
2024-01-22 22:43:07 +0900

    practice exception part1

--- exception/src/main/java/com/example/exception/contoller/ApiController.java
+++ exception/src/main/java/com/example/exception/contoller/ApiController.java
@@ -2,22 +2,30 @@ package com.example.exception.contoller;
 
 import com.example.exception.dto.User;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(@Size(min = 2)
+                    @RequestParam String name,
+
+                    @NotNull
+                    @Min(1)
+                    @RequestParam Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
-
-        int a = 10 + age;
 
         return user;
     }
