55fcc8beff037a4f9ca9364c1a5e1f5ab3913ff5
mani703 <mani703@naver.com>
2024-02-06 21:15:24 +0900

    practice RestTemplate Get Method

---
+++ client/src/main/java/com/example/client/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package com.example.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+
+    private String name;
+    private int age;
+}
